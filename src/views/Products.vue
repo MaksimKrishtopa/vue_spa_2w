952aa4a6db40c0c3e36a19ab7abe880b3d7098a5
MaksimKrishtopa <k.maks21@mail.ru>
2024-02-23 20:34:11 +0700

    Каталога, стилизация каталога и главной страницы

---
+++ src/views/Products.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="products">
+      <nav>
+      <div class="Logo"><img class="add-to-cart__img" src="../assets/Group 1.png" alt="Cart"></div>
+        <div class="nav-container">
+        
+        <p class="nav-links" v-show="store.state.user_token === null">
+          <router-link to="/registration">Регистрация</router-link>
+        </p>
+        <p class="nav-links" v-show="store.state.user_token === null">
+          <router-link to="/login">Войти</router-link>
+        </p>
+        <p class="nav-links" v-show="store.state.user_token !== null">
+          <a href="#" @click="store.commit('logout')">Выход</a>
+        </p>
+        <p class="nav-links" v-show="store.state.user_token !== null">
+          <router-link to="/cart">Корзина</router-link>
+        </p>
+        <p class="nav-links" v-show="store.state.user_token !== null">
+          <router-link to="/order">Заказы</router-link>
+        </p>
+        </div>
+      </nav>
+
+
+      <div class="product-list">
+        <div class="product" v-for="(product, index) in store.state.products" :key="product.id">
+          <h3>{{ product.name }}</h3>
+          <p>{{ product.description }}</p>
+          <p>Цена: {{ product.price }}</p>
+          <button @click="store.commit('addToCart', product)" v-show="store.state.user_token !== null"><img class="add-to-cart__img" src="../assets/корзина.svg" alt="Cart">В корзину</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  import store from "@/store";
+  
+  export default {
+    computed: {
+      store() {
+        return store
+      }
+    },
+    mounted(){
+      if (localStorage.token !== undefined && localStorage.token !== null) {
+        store.state.user_token = localStorage.token;
+      }
+      this.$store.commit('fetchProducts');
+    },
+  }
+  
+  </script>
+  
+  <style scoped>
+  .products {
+  max-width: 1200px;
+  margin: 0 auto;
+  color: #ececec;
+}
+
+nav {
+    display: flex;
+    padding: 5px;
+    background-color: rgb(79, 123, 206);
+    border-radius: 10px;
+    font-size: 24px;
+    box-shadow: 5px 5px 5px silver;
+    justify-content: space-between;
+}
+
+.nav-container{
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    padding-right: 30px;
+}
+
+.Logo {
+    padding-left: 30px;
+}
+
+.Logo img{
+    width: 100px;
+}
+
+.nav-links {
+  margin: 0;
+}
+
+.nav-links * {
+  text-decoration: none;
+  color: #ececec;
+}
+
+.nav-links *:hover {
+  transition: color 0.45s ease;
+  color: #d2d2d2;
+}
+
+.product-list {
+  margin-top: 40px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.product {
+    background-color: rgb(79, 123, 206);
+    border-radius: 5px;
+    width: 220px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 40px;
+    padding: 0px 20px 0px 20px;
+    box-shadow: 5px 5px 5px silver;
+}
+
+.product button {
+  width: 150px;
+  height: 40px;
+  margin-bottom: 30px;
+  background-color: rgb(79, 123, 206);
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  border: 1px solid #ececec;
+}
+
+.product button:hover {
+  background-color: rgb(50, 83, 145);
+}
+
+
+.add-to-cart__img {
+  width: 25px;
+}
+
+  </style>
